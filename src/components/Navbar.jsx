ea53f0ce2684f523210fbbcf9b0a5156638f76f2
muharrem gem <muharremgem28@gmail.com>
2022-10-10 19:57:48 +0300

    font bold

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="text-center text-white py-14 bg-blue-500 text-4xl">
+    <div className="text-center font-bold text-white py-14 bg-blue-500 text-4xl">
       LINKEDIN TOP 500 RECRUITER LİST
     </div>
   );
