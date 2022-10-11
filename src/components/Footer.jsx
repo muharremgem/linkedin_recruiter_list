0f42cc6565f928b5e6c706086dca9c71ba4b1917
muharrem gem <muharremgem28@gmail.com>
2022-10-11 17:32:08 +0300

    react-icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <div>
       <h6 className="flex  justify-end text-sm  text-purple-600 font-thin">
+        <GiSpiderWeb />
         <a href="https://www.muharremgem.com/" target="_blank">
-          <GiSpiderWeb /> Designed by Muharrem GEM
+          Designed by Muharrem GEM
         </a>
       </h6>
     </div>
